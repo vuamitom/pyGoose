d4332dc392ab21734089a8f328b7c67ff90f0b2c
Tam Vu <tamvu89@gmail.com>
2012-07-15 15:38:20 +0800

    remve bad tags

--- cleaner.py
+++ cleaner.py
@@ -16,20 +16,23 @@ class DocumentCleaner(object):
         self.xscript = etree.XPath("//script")
         self.xpara_span = etree.XPath("//p/span")
         self.xdropcap = etree.XPath("//span[contains(@class,'dropcap') or contains(@class,'drop-cap')]")
-        
+        #self.xdropcap = etree.XPath("//span[matches(@class,'(dropcap|drop-cap)')]")
 
     def clean(self, article):
         logging.info("Cleaning article")
         doc = article.doc
         self.cleanem(doc)
         self.rm_scriptstyle(doc)
+        self.rm_dropcaps(doc)
+        self.rm_para_spantags(doc)
+        self.clean_badtags(doc)
 
     def _replacewithtext(self, node):
         """replace an element with its text """
         parent = node.getparent()
         parent.remove(node)
         try:
-            prevsib = next(node.itersiblings(Preceding=True))
+            prevsib = next(node.itersiblings(preceding=True))
             prevsib.tail = node.text
         except StopIteration:
             parent.text = node.text
@@ -39,7 +42,7 @@ class DocumentCleaner(object):
         for node in emlist:
             images = self.ximg(node) 
             if(len(images) == 0):
-                self.replacewithtext(node)
+                self._replacewithtext(node)
 
         logging.info(str(len(emlist)) + " EM tags modified")
         return doc
@@ -65,8 +68,8 @@ class DocumentCleaner(object):
         """When a span tag nests in a paragraph tag"""
         spans = self.xpara_span(doc) 
         for span in spans:
-            paranode = span.getparent()
             #replace span with text 
+            self._replacewithtext(span)
 
 
     def rm_dropcaps(self, doc):
@@ -74,9 +77,85 @@ class DocumentCleaner(object):
         items = self.xdropcap(doc)
         for item in items:
             #replace 
-            self.replacewithtext(item)
+            self._replacewithtext(item)
 
     def clean_badtags(self, doc):
-        
-            
+        """remove sidebar, block comments, reviews ... """
+        noiselist = self.getnoisesource().getnoisyids()
+        noisyIds = self.cssselect(doc, "id", noiselist)
+        logging.info(str(len(noisyIds)) + " noisy ids removed ")
+        for ele in noisyIds:
+            ele.getparent().remove(ele)
+
+        noisyclasses = self.cssselect(doc, "class", noiselist)
+        logging.info(str(len(noisyclasses)) + " noisy classes removed ")
+        for ele in noisyclasses: 
+            ele.getparent().remove(ele)
+
+        noisynames = self.cssselect(doc, "name", noiselist)
+        logging.info(str(len(noisynames)) + " noisy names removed ")
+        for ele in noisynames:
+            ele.getparent().remove(ele)
+
+        #clean bad tags that match regex
+        pattern = self.getnoisesource().getnoisypatterns()
+        noisypatterns = self.regexselect(doc, "id", pattern)
+        logging.info(str(len(noisypatterns)) + " tag IDs match noisy patterns and removed ")
+        for ele in noisypatterns:
+            ele.getparent().remove(ele)
+
+        noisypatterns = self.regexselect(doc, "class", pattern)
+        logging.info(str(len(noisypatterns)) + " tag Classes match noisy patterns and removed ")
+        for ele in noisypatterns:
+            ele.getparent().remove(ele)
+
+
+    def regexselect(self, ele, attr, pattern):
+        """clean tags that has attr matches a pattern"""
+        regexpNS = "http://exslt.org/regular-expressions"
+        path = etree.XPath("//*[re:test(@"+attr+",'" + pattern + "','i')]", namespaces={'re':regexpNS})
+        return path(ele)
+
+    def getnoisesource(self):
+        return NoisyKeyProvider.instance()
+    
+    def cssselect(self, ele, attr, value):
+        """wrapper around selecting DOM. using XPath. Can switch to cssselect incase of more specific need such as ^=, $=, ~="""
+        terms = value.split("|")
+        func = lambda t : "contains(@" + attr + ",'" + t + "')" 
+        path = " or ".join(map(func, terms))
+        path = "//*[" + path + "]" 
+        filters = etree.XPath(path)
+        return filters(ele)
+
+
+from util import Singleton
+
+@Singleton
+class NoisyKeyProvider:
+    """singleton class to provide list of noisy kewords""" 
+    def __init__(self):
+        self.noisykeywords = "side|combx|retweet|mediaarticlerelated|menucontainer|navbar|comment|PopularQuestions|\
+        contact|foot|footer|Footer|footnote|cnn_strycaptiontxt|links|meta|scroll|shoutbox|sponsor\
+        |tags|socialnetworking|socialNetworking|cnnStryHghLght|cnn_stryspcvbx|inset|pagetools|post-attributes|welcome_form|contentTools2|the_answers|remember-tool-tip\
+        |communitypromo|runaroundLeft|subscribe|vcard|articleheadings|date|print|popup|author-dropdown|tools|socialtools|byline|konafilter|KonaFilter|breadcrumbs|fn|wp-caption-text"
+        #self.noisypatterns = []
+        #self.noisypatterns.append("[^-]facebook")
+        #self.noisypatterns.append("^caption$")
+        #self.noisypatterns.append("google")
+        #self.noisypatterns.append("^entry-more.*$")
+        #self.noisypatterns.append("[^-]twitter")
+        #optimize regex
+        self.noisypatterns = "^([^-]*(facebook|twitter)|(caption|entry-more.*)$)"
+
+    def getnoisyids(self):
+        return self.noisykeywords        
+
+    def getnoisyclasses(self):
+        return self.noisykeywords
+
+    def getnoisypatterns(self):
+        #return "(" + "|".join(self.noisypatterns) + ")"
+        return self.noisypatterns
+
 
--- util.py
+++ util.py
@@ -79,4 +79,23 @@ class URLHelper(object):
             logging.critical("Error in parsing url " + str(url))
             raise e
             
+
+class Singleton(object):
+    """Decorator class to make singleton structure"""
+    def __init__(self, decorated):
+        self._target = decorated
+
+    def instance(self):
+        if hasattr(self, '_instance'):
+            return self._instance
+        else:
+            self._instance = self._target()
+            return self._instance
         
+
+    def __call__(self):
+        raise TypeError('Singletons must be created using Instance() ')
+
+    def __instancecheck__(self,inst):
+        return isinstance(inst, self._target)
+
